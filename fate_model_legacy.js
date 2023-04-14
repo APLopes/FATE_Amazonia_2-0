91280395a9cf918bfd9af4d701b83c33344fa193
Wallace Vieira da Silva <49287183+wallyboy22@users.noreply.github.com>
2023-04-14 15:12:31 -0300

    atualização da mascara de floresta estavel

--- fate_model_legacy.js
+++ fate_model_legacy.js
@@ -10,7 +10,7 @@
 //
 //
 // Created by: Camila Silva, Aline Pontes e Wallace Silva
-// Last edited: 13 Apr 2023
+// Last edited: 14 Apr 2023
 //_______________________________________________________________________________
 // --- --- --- VERSION
 var version = 'v2-1-legacy';
@@ -44,7 +44,7 @@ postYearsScars.forEach(function(year){
 
 var annual_fire_freq_gte2 = annual_fire.updateMask(frequence_fire.gte(2)).gte(1),
     
-    mask_stable = ee.Image('projects/ee-seeg-brazil/assets/collection_10/v1/2_1_Mask_stable/SEEG_c10_v1_2020').eq(3).selfMask(),
+    mask_stable = ee.Image('projects/mapbiomas-workspace/SEEG/2023/c10/2_0_Mask_stable/SEEG_c10_v1_2020').eq(3).selfMask(),
     
     qcn = ee.ImageCollection('projects/mapbiomas-workspace/SEEG/2022/QCN/QCN_30m_BR_v2_0_1')
       .mosaic(),
