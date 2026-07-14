75fa8997fd6486beee16b739368b02c69e2356dc
wallyboy22 <49287183+wallyboy22@users.noreply.github.com>
2026-07-13 22:31:48 -0300

    Corrigir source paths no master.R para runtime do Code Ocean

--- code/R/master.R
+++ code/R/master.R
@@ -9,12 +9,12 @@
 # R version: 4.1.2
 
 cat("\n========== Script 1: Deforestation analyses (Fig 1) ==========\n")
-source("code/R/1.Script_Deforestation_analyses_Fig1.R")
+source("R/1.Script_Deforestation_analyses_Fig1.R")
 
 cat("\n========== Script 2: AGB change analyses (Fig 2) ==========\n")
-source("code/R/2.Script_AGB_change_analyses_Fig2.R")
+source("R/2.Script_AGB_change_analyses_Fig2.R")
 
 cat("\n========== Script 3: GEE model output (Fig 3) ==========\n")
-source("code/R/3.Script_GEE_model_output_Fig3.R")
+source("R/3.Script_GEE_model_output_Fig3.R")
 
 cat("\n========== All scripts completed ==========\n")
